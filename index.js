295528c10d48305dc113f78a680cd4c24ab730dd
thehexatown-husnain-umer <104353315+thehexatown-husnain-umer@users.noreply.github.com>
2023-04-08 04:49:40 +0500

    procfile

--- index.js
+++ index.js
@@ -122,7 +122,7 @@ if(result){
  
 });
 
-// Start server
-app.listen(3000, function () {
+const port = process.env.PORT
+app.listen(port, function () {
   console.log("Server started on port 3000");
 });
